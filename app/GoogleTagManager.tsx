c0e9ac2c7bf3a7dd682e04b80cb046215e0b74dc
CodeSandbox <support@codesandbox.io>
2024-01-17 13:50:50 +0000

    Initial commit

---
+++ app/GoogleTagManager.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Script from "next/script";
+import React from "react";
+
+export const GTM_ID = "GTM-MJ3629V5";
+
+const GoogleTagManager = () => {
+  return (
+    <>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', '${GTM_ID}');`}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleTagManager;
