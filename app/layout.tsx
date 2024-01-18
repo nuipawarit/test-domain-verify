6b420937d4e1d9f5ce3a1692acf927b506348bd7
Pawarit (Nhui) Chongkrichug <nightwsh58@gmail.com>
2024-01-18 15:03:08 +0000

    add children

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <GTM />
+        {children}
       </body>
       {/* <GoogleTagManager gtmId="GTM-MJ3629V5" /> */}
     </html>
